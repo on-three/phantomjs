b51df6777a9c1f28f852152c739e3f110fc3efe5
on-three <on.three.email@gmail.com>
2017-12-05 18:49:49 -0800

    Added arguments for page, selector.

--- snapshot.js
+++ snapshot.js
@@ -1,12 +1,33 @@
 var page = require('webpage').create();
+var system = require('system');
 
-page.open('http://google.com', function() {
+var args = system.args
+var url = 'http://google.com'
+var selector = '#hplogo'
+var file = 'google.png'
+
+if(args.length > 1)
+{
+  url = args[1];
+}
+if(args.length > 2)
+{
+  selector = args[2];
+}
+if(args.length > 3)
+{
+  file = args[3]
+}
+
+page.open(url, function() {
     // being the actual size of the headless browser
     page.viewportSize = { width: 768, height: 1024 };
-      
-    var clipRect = page.evaluate(function(){
-      return document.querySelector('#hplogo').getBoundingClientRect();
-    });
+    var s = "#hplogo"      
+    var clipRect = page.evaluate(function(s){
+      //return document.querySelector('#hplogo').getBoundingClientRect();
+      return document.querySelector(s).getBoundingClientRect();
+      //var selector = '#hplogo'
+    },s);
     page.clipRect = {
       top:    clipRect.top,
       left:   clipRect.left,
@@ -14,6 +35,6 @@ page.open('http://google.com', function() {
       height: clipRect.height
     };
       
-    page.render('google.png');
+    page.render(file);
       phantom.exit();
 });
